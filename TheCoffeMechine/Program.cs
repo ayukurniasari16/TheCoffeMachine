541f0b831e79edb8dea4f37010d1f4433338462a
ayukurniasari16 <ayukurnianuraga@gmail.com>
2020-03-30 23:10:02 +0700

    Add project files.

---
+++ TheCoffeMechine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coffe Kur");
+
+            CoffePowder coffe = new CoffePowder(100);
+            WaterGalon water = new WaterGalon(1000);
+            CoffeMechine yummycoffe = new CoffeMechine(coffe, water);
+
+      
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            String result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+ 
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+   
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+
+            Console.WriteLine("Checck " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("Check " + yummycoffe.checkAvailability());
+        }
+    }
+}
